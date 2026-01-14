9bee7ed6256a7fa3ddc38b58c9a9b3fa177410f8
AbdelAziz Mseddi <msedutent@gmail.com>
2026-01-14 23:35:17 +0100

    testing the cli

---
+++ cli/app.py
@@ -0,0 +1,23 @@
+import typer
+from rich.console import Console
+from rich.table import Table
+from rich.progress import Progress, SpinnerColumn
+
+app=typer.Typer(name="drose",
+                help="i am homelander, your savior:)",
+                no_args_is_help=True,
+                add_completion=False)
+console=Console()
+
+@app.command()
+def version():
+    """show version"""
+    console.print("[red]DROSE v0.1.0[/red]")
+
+@app.command()
+def test():
+    """testing command"""
+    console.print("[violet]wiiiiiw[/violet]")
+
+if __name__ == "__main__":
+    app()
