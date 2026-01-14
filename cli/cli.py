926f444c105e082cf32d0c72a1e8e37c93d4ca06
AbdelAziz Mseddi <msedutent@gmail.com>
2026-01-14 20:58:11 +0100

    refactor: restructure CLI with Typer command groups

--- cli/cli.py
+++
@@ -1,7 +0,0 @@
-import argparse 
-parser = argparse.ArgumentParser(
-    description="Download your favorite YouTube Music playlists in One Click"
-)
-parser.add_argument("playlist", help="Add a valid YouTube/YouTube Music playlist URL")
-parser.add_argument("--output", "-out", default="downloads", help="Where do you want to save the playlist?")
-args=parser.parse_args()
