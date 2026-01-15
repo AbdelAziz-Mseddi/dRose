1783fd61e7249a1c35438e5d0b65314c7f024c70
AbdelAziz Mseddi <msedutent@gmail.com>
2026-01-15 20:13:11 +0100

    cli and download function linked and working

--- cli/commands/playlist.py
+++ cli/commands/playlist.py
@@ -1,13 +1,18 @@
 import typer
-import core.downloader
+import core.playlist as down
+from rich.console import Console
 app=typer.Typer(
     name="playlist",
     help="i am the main character, THE PLAYLIST BOSS :p",
     no_args_is_help=True,
     add_completion=False
 )
+console=Console()
 
 @app.command()
-def download():
+def download(url : str = typer.Argument(..., help="lien URL de la playlist visée")):
     """download your favorite playlist"""
-    pass
+    console.print("[bold green]Starting download...🌹[/bold green]")
+    console.print(f"URL: {url}")
+    down.download_playlist(url)
+    console.print("[bold green]🌹 Download complete![/bold green]")
