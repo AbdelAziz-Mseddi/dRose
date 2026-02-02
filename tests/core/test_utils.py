bc16d4374ae9c26f3cafbbe8a84f2b4c460c4b5a
AbdelAziz Mseddi <msedutent@gmail.com>
2026-02-02 18:51:15 +0100

    little fix

--- tests/core/test_utils.py
+++ tests/core/test_utils.py
@@ -1,8 +1,8 @@
 from core import utils
 from pathlib import Path
 
-def test_create_folder(where):
-    root=where
+def test_create_folder(tmp_path):
+    root=tmp_path
     name="diane"
     nguyen=utils.create_folder(name,root)
     assert nguyen.exists() and nguyen.is_dir() and nguyen=="diane"
