bf3ac6cc1c8898162609de4ce9b521ad7be2aa1b
AbdelAziz Mseddi <msedutent@gmail.com>
2026-02-07 12:10:01 +0100

    mocking download_playlist

--- tests/core/test_playlist.py
+++ tests/core/test_playlist.py
@@ -11,12 +11,59 @@ def test_get_song_urls_from_playlist():
     for url in to_test:
         assert url.startswith("https://music.youtube.com/")
 
-def test_download_playlist(tmp_path):
+def test_download_playlist(tmp_path, monkeypatch):
     root=tmp_path
     url="https://music.youtube.com/playlist?list=PLVe3Pb0zUL05U3MfDdxMvyOHdwE7hYP2S&si=9i-hLHd9ydd6kPUc" #i don't want my code to crash
     output=root
+    music=["Nafs", "Vodka", "على باب السيما", "Dance with The Devil"]
+    # your usual mocking, sir?
+    # functions used in core/playlist.py
+    # i know i don't need to mock get_song_urls_from_playlist and get_playlist_info but no need go through them again
+    def mock_urls(url):
+        return [ "https://music.youtube.com/watch?v=lxBupPZBCb0&si=kjxqg5mVaPZE79vn", #NAFS
+                "https://music.youtube.com/watch?v=f7c88WJzQTY&si=yKt0JB3hWA8dPFsR", #vodka
+                "https://music.youtube.com/watch?v=kPouhuOAzi0&si=IQFmEuK_Lx67YEjO", #Raphinha
+                 "https://music.youtube.com/watch?v=Ef1fy2k_EYI&si=BPDzVDgJ02ADXiCD" #Dance With The Devil
+                ] 
+    def mock_info(url):
+        return {
+            "duration": 1031,
+            "uploader": "Free Palestine",
+            "title": "It's A Meee Mario",
+            "size": 4,
+            "tracks":[  ( "Nafs", 205, "EMP1RE" )
+                        ( "Vodka", 261, "Al Selem Band" ),
+                        ( "على باب السيما", 152, "Cairokee" ),
+                        ( "Dance with The Devil", 413, "Immoral Technique" )
+                      ]
+        }
+    class FakeYoutubeDL:
+        def __init__(self, opts):
+            self.opts=opts
+        # context manager protocol
+        def __enter__(self):
+            return self
+        def __exit__(self, exc_type, exc, tb):
+            pass
+        # methods used in core/downloader.py
+        def extract_info(self, url, download=True):
+            for track in music:
+                yield{
+                    "title": track,
+                    "ext": "mp3"
+                }
+        def prepare_filename(self, song):
+            for track in music:
+                path=tmp_path/track
+                path.touch()
+                yield str(path)
+    monkeypatch.setattr("core.playlist.download_playlist.get_song_urls_from_playlist", mock_urls)
+    monkeypatch.setattr("core.playlist.download_playlist.get_playlist_info", mock_info)
+    monkeypatch.setattr("core.downloader.yt_dlp.YoutubeDL", FakeYoutubeDL)
     playlist.download_playlist(url, str(output))
     playlist_path=tmp_path / "VHS"
     assert playlist_path.exists() and playlist_path.is_dir()
     for file in playlist_path.iterdir():
         assert file.is_file() and file.suffix==".mp3"
+#So when the devil wants to dance with you, you better say never
+#Because the dance with the devil might last you forever
